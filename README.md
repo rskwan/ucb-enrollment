22cd7b2816da81de70a2bbcc8177139b31d361d6
Ronald Kwan <rkwan@berkeley.edu>
2013-04-08 17:20:14 -0700

    initial commit. getInfo.py grabs math course information

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ ucb-enrollment
 ==============
 
 Tool for scraping enrollment information from the UCB Online Schedule of Classes.
+
+## Requirements
+
+* Python
+* pip
+* virtualenv
+
+The contents of 'requirements.txt':
+
+    argparse==1.2.1
+    beautifulsoup4==4.1.3
+    requests==1.2.0
+    wsgiref==0.1.2
---
+++ getInfo.py
@@ -0,0 +1,70 @@
+import re, requests
+from bs4 import BeautifulSoup
+# from depts import depts
+
+depts = ['MATH']
+
+def search_dept(dept):
+    payload = { 'p_term': 'FL',
+                'p_deptname': '-- Choose a Department Name --',
+                'p_classif': '-- Choose a Course Classification --',
+                'p_presuf': '-- Choose a Course Prefix/Suffix --',
+                'p_dept': dept }
+    r = requests.get('http://osoc.berkeley.edu/OSOC/osoc', params=payload)
+    analyze_html(r.text)
+
+    # get the total rows so we can keep searching
+    soup = BeautifulSoup(r.text)
+    tables = soup.find_all('table')
+
+    # this is the 'see next results' link
+    nextlink = str(tables[0].find_all('tr')[1].find_all('td')[1].br.a['href'])
+
+    # use regex to get the total rows out
+    match = re.search('p_total_rows=\d+', nextlink)
+    totalrows = int(match.group().split('=')[1])
+
+    # each page displays 100 rows (sections); index starts with 1, not 0
+    for i in range(1, (totalrows/100)):
+        payload['p_start_row'] = str(100*i + 1)
+        r = requests.get('http://osoc.berkeley.edu/OSOC/osoc', params=payload)
+        analyze_html(r.text)
+
+def analyze_html(txt):
+    soup = BeautifulSoup(txt)
+
+    # each section is displayed as a table
+    tables = soup.find_all('table')
+    sections = tables[1:(len(tables)-1)]
+
+    for section in sections:
+        rows = section.find_all('tr')
+
+        title = rows[0].find_all('td')[2].b.string
+        ccn = rows[5].find_all('td')[1].tt.contents[0]
+        try:
+            ccn = int(ccn)
+        except ValueError:
+            # sections with non-integer CCNs usually don't have enrollment info
+            continue
+
+        enrollinfo = rows[10].find_all('td')
+        enrollnums = enrollinfo[1].tt.string.split()[:3]
+        date = enrollinfo[0].b.string.split()[2][:-1]
+
+        data = {}
+        data['limit'] = int(enrollnums[0][6:])
+        data['enrolled'] = int(enrollnums[1][9:])
+        data['waitlist'] = int(enrollnums[2][9:])
+
+        # do something with the info
+        show_section(title, ccn, date, data)
+
+def show_section(title, ccn, date, data):
+    out = str(title).strip() + " (CCN " + str(ccn).strip() + "): Limit "
+    out += str(data['limit']) + ", Enrolled " + str(data['enrolled'])
+    out += ", Waitlist " + str(data['waitlist']) + " (" + str(date) + ")"
+    print out
+
+for dept in depts:
+    search_dept(dept)
